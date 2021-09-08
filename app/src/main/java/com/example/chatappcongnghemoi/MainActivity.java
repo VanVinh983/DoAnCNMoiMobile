2b872145ef4a70c79e4114762cd30a00c123b422
Phanthong2000 <phanthong2k000@gmail.com>
2021-09-08 16:10:28 +0700

    first commit

---
+++ app/src/main/java/com/example/chatappcongnghemoi/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.chatappcongnghemoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Animation topAnim,bottomAnim;
+    ImageView logo,slogan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_anim);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+        logo = findViewById(R.id.imgLogo);
+        slogan = findViewById(R.id.slogan);
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
