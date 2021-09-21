ce57154dcb3b8d4ab989ed4090b6156155b2029c
VanVinh983 <doanvinh983@gmail.com>
2021-09-21 21:57:23 +0700

    update personal introduce

---
+++ app/src/main/java/com/example/chatappcongnghemoi/UpdateIntroduce.java
@@ -0,0 +1,41 @@
+package com.example.chatappcongnghemoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class UpdateIntroduce extends AppCompatActivity {
+    private EditText input_introduce;
+    private Button btn_save;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_introduce);
+        getSupportActionBar().hide();
+
+        input_introduce = findViewById(R.id.input_updateintroduce_introduce);
+        btn_save = findViewById(R.id.btn_updateintroduce_save);
+
+        Intent intent = getIntent();
+        String introduce = intent.getStringExtra("introduce");
+
+        if (!introduce.equals("")){
+            input_introduce.setText(introduce);
+        }
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = input_introduce.getText().toString();
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("result", result);
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/chatappcongnghemoi/activities/Personal.java
+++ app/src/main/java/com/example/chatappcongnghemoi/activities/Personal.java
@@ -1,27 +1,39 @@
 package com.example.chatappcongnghemoi.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.chatappcongnghemoi.R;
+import com.example.chatappcongnghemoi.UpdateIntroduce;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Calendar;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Personal extends AppCompatActivity implements View.OnClickListener {
     private EditText input_name, input_email, input_gender, input_yearOfBirth, input_numberPhone, input_address;
     private BottomNavigationView bottomNavigationView;
     private Button btn_update_info;
-    private DatePickerDialog.OnDateSetListener setListener;
+    private CircleImageView imageView_Avatar;
+    private TextView txt_introduce;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +49,8 @@ public class Personal extends AppCompatActivity implements View.OnClickListener 
         input_gender = findViewById(R.id.input_personal_gender);
         input_yearOfBirth = findViewById(R.id.input_personal_yearOfBirth);
         btn_update_info = findViewById(R.id.btn_personal_update);
+        imageView_Avatar = findViewById(R.id.image_personal_avatar);
+        txt_introduce = findViewById(R.id.txt_personal_introduce);
 
         //set edit text can't input letter
 
@@ -88,13 +102,31 @@ public class Personal extends AppCompatActivity implements View.OnClickListener 
         });
         //add onclick listener for button
         btn_update_info.setOnClickListener(this);
+
+        // event dialog avatar
+        imageView_Avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialogAvatar(Gravity.CENTER);
+            }
+        });
+
+        //event introduce
+        txt_introduce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Personal.this, UpdateIntroduce.class);
+                intent.putExtra("introduce", txt_introduce.getText().toString());
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btn_personal_update:{
-                if (btn_update_info.getText().equals("Cập nhật thông tin")){
+            case R.id.btn_personal_update: {
+                if (btn_update_info.getText().equals("Cập nhật thông tin")) {
                     btn_update_info.setText("Lưu");
                     //set edit text can input letter
                     input_gender.setEnabled(true);
@@ -103,8 +135,52 @@ public class Personal extends AppCompatActivity implements View.OnClickListener 
                     input_address.setEnabled(true);
                     input_numberPhone.setEnabled(true);
                     input_yearOfBirth.setEnabled(true);
-                }else {
+                } else {
                     btn_update_info.setText("Cập nhật thông tin");
+                    input_gender.setEnabled(false);
+                    input_name.setEnabled(false);
+                    input_email.setEnabled(false);
+                    input_address.setEnabled(false);
+                    input_numberPhone.setEnabled(false);
+                    input_yearOfBirth.setEnabled(false);
+                }
+            }
+        }
+    }
+
+    private void openDialogAvatar(int gravity){
+         final Dialog dialog = new Dialog(Personal.this);
+         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+         dialog.setContentView(R.layout.layout_dialog_avatar_personal);
+
+         Window window = dialog.getWindow();
+         if (window==null){
+             return;
+         }
+
+         window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+         window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+         WindowManager.LayoutParams windowAttributes = window.getAttributes();
+         windowAttributes.gravity = gravity;
+         window.setAttributes(windowAttributes);
+
+         if (Gravity.CENTER == gravity){
+             dialog.setCancelable(true);
+         }else {
+             dialog.setCancelable(false);
+         }
+         dialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1){
+            if (resultCode == RESULT_OK){
+                String result = data.getStringExtra("result");
+                if (!result.equals("")){
+                    txt_introduce.setText(result);
                 }
             }
         }
