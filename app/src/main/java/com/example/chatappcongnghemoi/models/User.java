f7ac88ed8cae1306678ad2e4bfe6848b939167cb
thanhliem-SE <thanhliem2n@gmail.com>
2021-10-01 12:58:50 +0700

    Add some property for User Model

--- app/src/main/java/com/example/chatappcongnghemoi/models/User.java
+++ app/src/main/java/com/example/chatappcongnghemoi/models/User.java
@@ -4,7 +4,38 @@ public class User {
     private String userId;
     private String userName;
     private String avatar;
+    private String phone;
+
+    //User có online hay không
+    private boolean isOnline;
+    //Tài khoản có bị khóa hay không
     private boolean isActive;
+    //Ảnh bìa bên trang cá nhân
+    private String background;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isOnline() {
+        return isOnline;
+    }
+
+    public void setOnline(boolean online) {
+        isOnline = online;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public void setBackground(String background) {
+        this.background = background;
+    }
 
     public String getUserId() {
         return userId;
@@ -44,7 +75,10 @@ public class User {
                 "userId='" + userId + '\'' +
                 ", userName='" + userName + '\'' +
                 ", avatar='" + avatar + '\'' +
+                ", phone='" + phone + '\'' +
+                ", isOnline=" + isOnline +
                 ", isActive=" + isActive +
+                ", background='" + background + '\'' +
                 '}';
     }
 }
